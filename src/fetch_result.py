77061325914990a58ea3e943120d162359ca7ca0
parthokr <partho.rajvor2016@gmail.com>
2022-02-05 23:30:43 +0600

    Raising exception on unsuccessful response

--- src/fetch_result.py
+++ src/fetch_result.py
@@ -5,7 +5,10 @@ import re
 def get_result(exam, year, board, roll, reg):
     s = requests.Session()
 
-    body = s.get('http://www.educationboardresults.gov.bd/index.php?err=103').text
+    try:
+        body = s.get('http://www.educationboardresults.gov.bd/index.php?err=103').text
+    except:
+        raise Exception("Website is down")
     augend_addend = re.findall(r'<td align=\"left\" valign=\"middle\">(\d+) \+ (\d+)</td>', body)[0]
     augend_addend = [int(i) for i in augend_addend]
     payload = {
@@ -20,8 +23,10 @@ def get_result(exam, year, board, roll, reg):
         'button2': 'Submit'
     }
 
-    result = s.post('http://www.educationboardresults.gov.bd/result.php', data=payload).text
-
+    try:
+        result = s.post('http://www.educationboardresults.gov.bd/result.php', data=payload).text
+    except:
+        raise Exception("Website is down")
     name = re.findall(r'<td width=\"39%\" align=\"left\" valign=\"middle\" bgcolor=\"#EEEEEE\">([\w\s\.]+)</td>',
                       result)
     courses = re.findall(r'<td align=\"left\" valign=\"middle\" bgcolor=\"#EEEEEE\">([\w\s\+\.,&\-]+)</td>', result)
