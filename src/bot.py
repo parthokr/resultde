24f22adc07fe78b24125ef08f31c791c60fb9f5c
Partho Kumar Rajvor <partho.rajvor2016@gmail.com>
2022-02-05 11:42:05 +0600

    Removed dotenv

--- src/bot.py
+++ src/bot.py
@@ -2,14 +2,14 @@ import os
 import pdfkit
 
 import telegram.ext
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler
 
 from fetch_result import get_result
 
-load_dotenv()
-TOKEN = os.environ.get("TOKEN")
+# load_dotenv()
+TOKEN = os.environ.get("RESULTDE_TOKEN")
 
 import logging
 
